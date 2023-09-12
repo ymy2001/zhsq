476f542302c73d3475c23a2dad053e3378ee74ab
ymyacc <2360302452@qq.com>
2023-09-12 08:49:33 +0800

    创建xml映射文件

---
+++ src/test/java/com/zhsq/service/impl/FeeServiceImplTest.java
@@ -0,0 +1,9 @@
+package com.zhsq.service.impl;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class FeeServiceImplTest {
+
+}
